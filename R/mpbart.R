bfbee5b385bc39f78aaef59e49903db182903048
Bereket Kindo <bpkindo@gmail.com>
2016-01-01 15:56:19 -0500

    documentation in progress

--- R/mpbart.R
+++ R/mpbart.R
@@ -8,13 +8,17 @@ mpbart <- function(formula, train.data, test.data = NULL, base = NULL,
 #'Multinomial Probit Bayesian Additive Regression Trees
 #'
 #'Multinomial probit modeling using Bayesian Additive Regression Trees,
-#'@param formula choice ~ demographic covariates. If there are no, demographic variables use  ~ 1,
-#'@param train.data Training Data in wide format,
+#'@param formula response ~ choice speccific covariates | demographic covariates. If there are no, demographic variables use  response ~ choice specific covariates| ~ 1. If there are no choice specific covariates, use  response ~ 1 | demographic covariates
+#'@param train.data Training Data in wide format (for details on wide format, see documentation in R package \pkg{mlogit}),
 #'@param test.data Test Data in wide format, typically without the response,
-#'@param base Base choic. Default is the highest class/choice,
-#'@param varying the indexes of the variables that are alternative specific,
-#'@param sep the seperator of the variable name and the alternative name,
-#'@param Prior List of Priors for MPBART: e.g., Prior = list(nu=p+2,  V= diag(p - 1), ntrees=200,  kfac=2.0,  pbd=1.0, pb=0.5 , beta = 2.0, alpha = 0.95, nc = 100, priorindep = 0,  minobsnode = 10)
+#'@param base base choice. Default is the highest class/choice,
+#'@param varying The indeces of the variables that are alternative specific,
+#'@param sep The seperator of the variable name and the alternative name in the choice specific covariates. For example a covariate name variabl1.choice1 indicates a separator of dot (.).
+#'@param Prior List of Priors for MPBART: e.g., Prior = list(nu=p+2,  V= diag(p - 1), ntrees=200,  kfac=2.0,  pbd=1.0, pb=0.5 , beta = 2.0, alpha = 0.95, nc = 100, priorindep = FALSE,  minobsnode = 10).
+#'The comonents of Prior are
+#' \itemize{
+#'\item nu 
+#'}
 #'@param Mcmc List of MCMC starting values, burn-in ...: e.g.,     list(sigma0 = diag(p - 1), keep = 1, burn = 100, ndraws = 1000, keep_sigma_draws=FALSE)
 #'@param seedvalue random seed value, default of 99 will be used if null,
 #'@return class_prob_train training data choice/class probabilities,
